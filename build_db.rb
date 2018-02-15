bca270943880adbbe0035b4bb160c3f908b592b1
custompro98 <mitchjoa@gmail.com>
2018-02-15 07:50:11 -0600

    Add slides for updating

--- build_db.rb
+++ build_db.rb
@@ -1,18 +1,18 @@
 `sqlite3 sample_db.sqlite3 "DROP TABLE IF EXISTS posts"`
 `sqlite3 sample_db.sqlite3 "DROP TABLE IF EXISTS authors"`
 
-`sqlite3 sample_db.sqlite3 "CREATE TABLE posts(id INTEGER, author_id INTEGER, title STRING, content STRING)"`
-`sqlite3 sample_db.sqlite3 "CREATE TABLE authors(id INTEGER, name STRING)"`
+`sqlite3 sample_db.sqlite3 "CREATE TABLE posts(id INTEGER PRIMARY KEY AUTOINCREMENT, author_id INTEGER, title STRING, content STRING, deleted INTEGER DEFAULT false)"`
+`sqlite3 sample_db.sqlite3 "CREATE TABLE authors(id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)"`
 
 (1..50).each do |n|
-  `sqlite3 sample_db.sqlite3 "INSERT INTO posts(id, author_id, title, content) VALUES (#{n}, #{(n % 8) + 1}, 'Title #{n}', 'Content #{n}')"`
+  `sqlite3 sample_db.sqlite3 "INSERT INTO posts(author_id, title, content) VALUES (#{(n % 8) + 1}, 'Title #{n}', 'Content #{n}')"`
 end
 
-`sqlite3 sample_db.sqlite3 "INSERT INTO authors(id, name) VALUES (1, 'Mitch Joa')"`
-`sqlite3 sample_db.sqlite3 "INSERT INTO authors(id, name) VALUES (2, 'Danny Davidson')"`
-`sqlite3 sample_db.sqlite3 "INSERT INTO authors(id, name) VALUES (3, 'Kuu Hubbard')"`
-`sqlite3 sample_db.sqlite3 "INSERT INTO authors(id, name) VALUES (4, 'Thomas Huang')"`
-`sqlite3 sample_db.sqlite3 "INSERT INTO authors(id, name) VALUES (5, 'Adam Pogwizd')"`
-`sqlite3 sample_db.sqlite3 "INSERT INTO authors(id, name) VALUES (6, 'Noah Conley')"`
-`sqlite3 sample_db.sqlite3 "INSERT INTO authors(id, name) VALUES (7, 'Iana Gandziuk')"`
-`sqlite3 sample_db.sqlite3 "INSERT INTO authors(id, name) VALUES (8, 'Tom Benneche')"`
+`sqlite3 sample_db.sqlite3 "INSERT INTO authors(name) VALUES ('Mitch Joa')"`
+`sqlite3 sample_db.sqlite3 "INSERT INTO authors(name) VALUES ('Danny Davidson')"`
+`sqlite3 sample_db.sqlite3 "INSERT INTO authors(name) VALUES ('Kuu Hubbard')"`
+`sqlite3 sample_db.sqlite3 "INSERT INTO authors(name) VALUES ('Thomas Huang')"`
+`sqlite3 sample_db.sqlite3 "INSERT INTO authors(name) VALUES ('Adam Pogwizd')"`
+`sqlite3 sample_db.sqlite3 "INSERT INTO authors(name) VALUES ('Noah Conley')"`
+`sqlite3 sample_db.sqlite3 "INSERT INTO authors(name) VALUES ('Iana Gandziuk')"`
+`sqlite3 sample_db.sqlite3 "INSERT INTO authors(name) VALUES ('Tom Benneche')"`
